56f761bef5462807a30cad62f0bf118012734f3e
Jacob Torrey <jacob@thinkst.com>
2025-12-10 13:01:57 +1300

    Add specific ParseError type to except

--- xml.py
+++ xml.py
@@ -3,5 +3,5 @@ import xml.etree.ElementTree as ET
 def FuzzerRunOne(FuzzerInput):
     try:
         tree = ET.fromstring(FuzzerInput.decode("utf-8", "replace"))
-    except:
+    except ET.ParseError:
         pass
