8c65320695784e1678a85e442cd7f1ca66ef0215
Seth Michael Larson <seth@python.org>
2025-12-23 09:38:23 -0600

    Add hypothesis harness, tarfile strategies

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all : fuzzer-html fuzzer-email fuzzer-httpclient fuzzer-json fuzzer-difflib fuzzer-csv fuzzer-decode fuzzer-ast fuzzer-tarfile fuzzer-zipfile fuzzer-re fuzzer-configparser fuzzer-tomllib fuzzer-plistlib fuzzer-xml
+all : fuzzer-html fuzzer-email fuzzer-httpclient fuzzer-json fuzzer-difflib fuzzer-csv fuzzer-decode fuzzer-ast fuzzer-tarfile fuzzer-tarfile-hypothesis fuzzer-zipfile fuzzer-re fuzzer-configparser fuzzer-tomllib fuzzer-plistlib fuzzer-xml
 
 PYTHON_CONFIG_PATH=$(CPYTHON_INSTALL_PATH)/bin/python3-config
 CXXFLAGS += $(shell $(PYTHON_CONFIG_PATH) --cflags)
@@ -26,6 +26,8 @@ fuzzer-zipfile:
 	clang++ $(CXXFLAGS) $(LIB_FUZZING_ENGINE) -std=c++17 fuzzer.cpp -DPYTHON_HARNESS_PATH="\"zipfile.py\"" -ldl $(LDFLAGS) -o fuzzer-zipfile
 fuzzer-tarfile:
 	clang++ $(CXXFLAGS) $(LIB_FUZZING_ENGINE) -std=c++17 fuzzer.cpp -DPYTHON_HARNESS_PATH="\"tarfile.py\"" -ldl $(LDFLAGS) -o fuzzer-tarfile
+fuzzer-tarfile-hypothesis:
+	clang++ $(CXXFLAGS) $(LIB_FUZZING_ENGINE) -std=c++17 fuzzer.cpp -DPYTHON_HARNESS_PATH="\"tarfile_hypothesis.py\"" -ldl $(LDFLAGS) -o fuzzer-tarfile-hypothesis
 fuzzer-configparser:
 	clang++ $(CXXFLAGS) $(LIB_FUZZING_ENGINE) -std=c++17 fuzzer.cpp -DPYTHON_HARNESS_PATH="\"configparser.py\"" -ldl $(LDFLAGS) -o fuzzer-configparser
 fuzzer-tomllib:
---
+++ tarfile_hypothesis.py
@@ -0,0 +1,95 @@
+import io
+import tarfile
+
+from hypothesis import given
+from hypothesis import strategies as st
+
+
+def utf8_text(*, max_size: int, min_size: int = 0) -> st.SearchStrategy[str]:
+    """Returns UTF-8 text that, when encoded to bytes,
+    is within the size restrictions.
+    """
+    return st.text(min_size=min_size, max_size=max_size).filter(
+        lambda s: min_size <= len(s.encode("utf-8")) <= max_size
+    )
+
+
+def tar_integers(
+    *, format: int, digits: int = 1, allow_negative: bool = False
+) -> st.SearchStrategy[tuple[io.BytesIO, tarfile.TarFile]]:
+    """tar has a unique way of encoding integers that is format-dependent
+    and based on the number of "digits" allowed for a value.
+    """
+    if digits <= 0:
+        raise ValueError("Digits must be greater than one.")
+    if format == tarfile.GNU_FORMAT:
+        min_value = -(256 ** (digits - 1)) if allow_negative else 0
+        max_value = (256 ** (digits - 1)) - 1
+    else:
+        min_value = 0
+        max_value = (4**digits) - 1
+    return st.integers(min_value=min_value, max_value=max_value)
+
+
+@st.composite
+def tar_archives(draw):
+    buf = io.BytesIO()
+    format = draw(
+        st.sampled_from((tarfile.GNU_FORMAT, tarfile.PAX_FORMAT, tarfile.USTAR_FORMAT))
+    )
+    tar = tarfile.TarFile(fileobj=buf, format=format, mode="w")
+    types = list(tarfile.REGULAR_TYPES)
+
+    for _ in range(draw(st.integers(min_value=1, max_value=10))):
+        info = tarfile.TarInfo(
+            name=draw(utf8_text(min_size=1, max_size=tarfile.LENGTH_NAME))
+        )
+        if draw(st.booleans()):
+            fileobj = io.BytesIO(draw(st.binary(min_size=0, max_size=0xFFFFFFFF)))
+        else:
+            fileobj = None
+
+        info.type = draw(st.sampled_from(types))
+        info.mode = draw(tar_integers(format=format, digits=8))
+        info.uid = draw(tar_integers(format=format, digits=8))
+        info.gid = draw(tar_integers(format=format, digits=8))
+        info.mtime = draw(tar_integers(format=format, digits=12))
+        info.devmajor = draw(tar_integers(format=format, digits=8))
+        info.devminor = draw(tar_integers(format=format, digits=8))
+
+        if draw(st.booleans()):
+            info.linkname = draw(utf8_text(min_size=1, max_size=tarfile.LENGTH_LINK))
+
+        def maybe_set_pax_header(obj, name, value):
+            if draw(st.booleans()):
+                obj.pax_headers[name] = value
+
+        if format == tarfile.PAX_FORMAT:
+            maybe_set_pax_header(info, "uname", draw(st.text(max_size=32)))
+            maybe_set_pax_header(info, "gname", draw(st.text(max_size=32)))
+            maybe_set_pax_header(
+                info,
+                "path",
+                draw(utf8_text(min_size=1, max_size=tarfile.LENGTH_NAME)),
+            )
+            maybe_set_pax_header(
+                info,
+                "linkpath",
+                draw(utf8_text(min_size=1, max_size=tarfile.LENGTH_LINK)),
+            )
+
+        tar.addfile(info, fileobj=fileobj)
+
+    return buf, tar
+
+
+@given(tar_archives())
+def tar_archive_fuzz_target(buf_tar: tuple[io.BytesIO, tarfile.TarFile]) -> None:
+    buf, tar1 = buf_tar
+    tar2 = tarfile.TarFile(fileobj=buf)
+    # Assert that tar files round-trip.
+    assert list(tar1.getmembers()) == list(tar2.getmembers())
+
+
+# Exposes the Hypothesis fuzz target for integrating with OSS-Fuzz.
+FuzzerRunOne = tar_archive_fuzz_target.hypothesis.fuzz_one_input
