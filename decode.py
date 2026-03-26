2fa45bf030804aa42d8a4d41b4439b8c6cf82e5c
Stan Ulbrych <stan@ulbrych.org>
2026-03-26 20:25:04 +0000

    drop old approach altogether

--- decode.py
+++ decode.py
@@ -5,13 +5,8 @@ ALL_CODECS = sorted(set(aliases.values()))
 def FuzzerRunOne(FuzzerInput):
     if len(FuzzerInput) < 2:
         return
-    if FuzzerInput[0] & 1:
-        codec = ALL_CODECS[FuzzerInput[1] % len(ALL_CODECS)]
-        data = FuzzerInput[2:]
-    else:
-        l = len(FuzzerInput) // 2
-        codec = FuzzerInput[l:].decode("utf-8", "replace").strip()
-        data = FuzzerInput[:l]
+    codec = ALL_CODECS[FuzzerInput[0] % len(ALL_CODECS)]
+    data = FuzzerInput[1:]
     try:
         data.decode(codec)
     except SystemError:
