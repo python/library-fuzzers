3381a08b33adcee0414b2c4ed484c2b9bf457c9a
Adam Korczynski <adam@adalogics.com>
2026-04-02 12:20:22 +0100

    remove call to json.loads
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- fuzz_json_decode.py
+++ fuzz_json_decode.py
@@ -12,15 +12,13 @@ def FuzzerRunOne(FuzzerInput):
     if len(FuzzerInput) < 1 or len(FuzzerInput) > 0x100000:
         return
     fdp = FuzzedDataProvider(FuzzerInput)
-    target = fdp.ConsumeIntInRange(LOADS, DECODER_RAW_DECODE)
+    target = fdp.ConsumeIntInRange(DECODER_DECODE, DECODER_RAW_DECODE)
     n = fdp.ConsumeIntInRange(1, min(fdp.remaining_bytes(), 10000)) if fdp.remaining_bytes() > 0 else 0
     if n == 0:
         return
     s = fdp.ConsumeBytes(n).decode('latin-1')
     try:
-        if target == LOADS:
-            json.loads(s)
-        elif target == DECODER_DECODE:
+        if target == DECODER_DECODE:
             dec = json.JSONDecoder()
             dec.decode(s)
         elif target == DECODER_RAW_DECODE:
