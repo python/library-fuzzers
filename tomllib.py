b3bb5604242c6236125c27f2ecc3de1ef5307c22
Stan Ulbrych <stan@python.org>
2026-04-05 15:33:43 +0100

    `tomllib` may raise exceptions other than `TOMLDecodeError`

--- tomllib.py
+++ tomllib.py
@@ -3,6 +3,14 @@ import tomllib
 def FuzzerRunOne(FuzzerInput):
     try:
         tomllib.loads(FuzzerInput.decode("utf-8", "replace"))
+    except KeyError:
+        return
+    except RecursionError:
+        return
     except tomllib.TOMLDecodeError:
+        return
+    except TypeError:
+        return
+    except ValueError:
         return
 
