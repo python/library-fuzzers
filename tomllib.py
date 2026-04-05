c99dc5e16309fff8dece9dd3f71bf60a18f5d0dc
Stan Ulbrych <stan@python.org>
2026-04-05 16:52:11 +0100

    Pablo's nit

--- tomllib.py
+++ tomllib.py
@@ -1,16 +1,11 @@
 import tomllib
 
+POSSIBLE_EXCEPTIONS = (KeyError, RecursionError, tomllib.TOMLDecodeError,
+                       TypeError, ValueError)
+
 def FuzzerRunOne(FuzzerInput):
     try:
         tomllib.loads(FuzzerInput.decode("utf-8", "replace"))
-    except KeyError:
-        return
-    except RecursionError:
-        return
-    except tomllib.TOMLDecodeError:
-        return
-    except TypeError:
-        return
-    except ValueError:
+    except POSSIBLE_EXCEPTIONS:
         return
 
